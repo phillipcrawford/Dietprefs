dc49e39440c7d6101c942198bd3d859c2f1e3842
phillipcrawford <phillip.crawford3@gmail.com>
2025-08-09 19:49:16 -0600

    first commit

---
+++ app/src/main/java/com/example/dietprefs/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.dietprefs.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val dietprefsGrey = Color(0xFF333333)    // dark grey
+val selectedGrey = Color(0xFF595959)     // lighter grey
+val aiLRed = Color(0xFFEE6C6C) //maybe it was onto something?
+val dietprefsTeal2 = Color(0xFF005B5B)
+val user1Red = Color(0xFFE06666)
+val user2Magenta = Color(0xFFE0B3FF)
+val user2Color = Color(0xFFFF77FF)
+val dietprefsTeal = Color(0xFF004D4D)
+val selectedTeal = Color(0xFF008080)
