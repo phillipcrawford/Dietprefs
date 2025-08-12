4f19b4a183e19233024d0d230da277380575408a
phillipcrawford <phillip.crawford3@gmail.com>
2025-08-11 23:54:59 -0600

    dividing line looks goodb

--- app/src/main/java/com/example/dietprefs/ui/screens/SearchResultsScreen.kt
+++ app/src/main/java/com/example/dietprefs/ui/screens/SearchResultsScreen.kt
@@ -123,7 +123,7 @@ fun SearchResultsScreen(
                         .clickable { sharedViewModel.updateSortState(SortColumn.VENDOR_RATING) }
                 ) {
                     SortableHeader(
-                        text = "Vendeor",
+                        text = "Vendor",
                         column = SortColumn.VENDOR_RATING,
                         currentSortState = sortState
                     )
