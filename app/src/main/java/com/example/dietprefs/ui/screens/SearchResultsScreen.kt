964cb6d28e1890cc20879eafd4982cd50370f5cc
phillipcrawford <phillip.crawford3@gmail.com>
2025-08-14 23:57:42 -0600

    vendorseqmv

--- app/src/main/java/com/example/dietprefs/ui/screens/SearchResultsScreen.kt
+++ app/src/main/java/com/example/dietprefs/ui/screens/SearchResultsScreen.kt
@@ -123,7 +123,7 @@ fun SearchResultsScreen(
                         .clickable { sharedViewModel.updateSortState(SortColumn.VENDOR_RATING) }
                 ) {
                     SortableHeader(
-                        text = "Vendowemre",
+                        text = "Vendovwemre",
                         column = SortColumn.VENDOR_RATING,
                         currentSortState = sortState
                     )
