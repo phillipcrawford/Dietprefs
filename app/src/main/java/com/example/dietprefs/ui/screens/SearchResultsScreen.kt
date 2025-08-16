3bc0973432f9a708fb005dc89279b5247ffa5fa4
phillipcrawford <phillip.crawford3@gmail.com>
2025-08-15 23:57:50 -0600

    vendohrgh

--- app/src/main/java/com/example/dietprefs/ui/screens/SearchResultsScreen.kt
+++ app/src/main/java/com/example/dietprefs/ui/screens/SearchResultsScreen.kt
@@ -123,7 +123,7 @@ fun SearchResultsScreen(
                         .clickable { sharedViewModel.updateSortState(SortColumn.VENDOR_RATING) }
                 ) {
                     SortableHeader(
-                        text = "Vendhgor",
+                        text = "Vendhhgor",
                         column = SortColumn.VENDOR_RATING,
                         currentSortState = sortState
                     )
