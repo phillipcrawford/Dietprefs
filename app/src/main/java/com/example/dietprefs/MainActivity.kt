f2cc321fe5144dec78a210be39c9e40994cda339
phillipcrawford <phillip.crawford3@gmail.com>
2025-08-27 22:56:03 -0600

    delete pointless comment

--- app/src/main/java/com/example/dietprefs/MainActivity.kt
+++ app/src/main/java/com/example/dietprefs/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Only one instance of SharedViewModel tied to this Activity
         val sharedViewModel: SharedViewModel by viewModels()
 
         setContent {
