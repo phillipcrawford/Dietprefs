7586e58bff8b751cacb66d09fca5d80c2d713f17
phillipcrawford <phillip.crawford3@gmail.com>
2025-08-27 22:55:45 -0600

    delete unused import5

--- app/src/main/java/com/example/dietprefs/MainActivity.kt
+++ app/src/main/java/com/example/dietprefs/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.navigation.compose.rememberNavController
 import com.example.dietprefs.ui.navigation.AppNavGraph
-import com.example.dietprefs.ui.screens.PreferenceScreen
 import com.example.dietprefs.ui.screens.SearchResultsScreen
 import com.example.dietprefs.viewmodel.SharedViewModel
 import com.example.dietprefs.ui.theme.DietprefsTheme
