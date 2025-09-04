78b12b4ac8b3005ebda6803373031f7e2488bb2d
phillipcrawford <phillip.crawford3@gmail.com>
2025-09-03 23:57:08 -0600

    2

--- app/src/main/java/com/example/dietprefs/ui/screens/SearchResultsScreen.kt
+++ app/src/main/java/com/example/dietprefs/ui/screens/SearchResultsScreen.kt
@@ -139,7 +139,7 @@ fun SearchResultsScreen(
                             fontSize = 11.sp, // Slightly smaller
                             color = Color.White,
                             modifier = Modifier
-                                .padding(top = 243.dp)
+                                .padding(top = 2.dp)
                                 .align(Alignment.End)
                         )
                     } else {
